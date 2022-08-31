074fd2ca72d8555c337dd3352cb634caa22a68bd
Nicole Watts <nicolewatts@Firefly.local>
2022-08-30 23:43:18 -0600

    working setup scrpt

--- setup.sh
+++ setup.sh
@@ -1,24 +1,169 @@
 #!/bin/bash
 
-# Installs dotfiles
+# this symlinks all the dotfiles to ~/
+# This is an extension of Paul Irish's scripts but with some of my own features
 
-realpath() {
-    [[ $1 = /* ]] && echo "$1" || echo "$PWD/${1#./}"
+
+answer_is_yes() {
+    [[ "$REPLY" =~ ^[Yy]$ ]] \
+        && return 0 \
+        || return 1
 }
 
-safe_link(){
-    local src="$1"
-    local dest="$HOME/`basename $src`"
-    [ ! -e "$dest" ] && ln -sf "$src" "$dest"
+ask() {
+    print_question "$1"
+    read
 }
 
-# Get root
-SCRIPT_PATH=`realpath $0`
-DOTFILES=`dirname $SCRIPT_PATH`
+ask_for_confirmation() {
+    print_question "$1 (y/n) "
+    read -n 1
+    printf "\n"
+}
 
-# Install bin scripts
-safe_link "$DOTFILES/bin"
-# Install other dotfiles
-for f in `ls -A $DOTFILES/dotfiles`; do
-    safe_link "$DOTFILES/dotfiles/$f"
-done
+ask_for_sudo() {
+
+    # Ask for the administrator password upfront
+    sudo -v
+
+    # Update existing `sudo` time stamp until this script has finished
+    # https://gist.github.com/cowboy/3118588
+    while true; do
+        sudo -n true
+        sleep 60
+        kill -0 "$$" || exit
+    done &> /dev/null &
+
+}
+
+cmd_exists() {
+    [ -x "$(command -v "$1")" ] \
+        && printf 0 \
+        || printf 1
+}
+
+execute() {
+    $1 &> /dev/null
+    print_result $? "${2:-$1}"
+}
+
+get_answer() {
+    printf "$REPLY"
+}
+
+get_os() {
+
+    declare -r OS_NAME="$(uname -s)"
+    local os=""
+
+    if [ "$OS_NAME" == "Darwin" ]; then
+        os="osx"
+    elif [ "$OS_NAME" == "Linux" ] && [ -e "/etc/lsb-release" ]; then
+        os="ubuntu"
+    fi
+
+    printf "%s" "$os"
+
+}
+
+is_git_repository() {
+    [ "$(git rev-parse &>/dev/null; printf $?)" -eq 0 ] \
+        && return 0 \
+        || return 1
+}
+
+mkd() {
+    if [ -n "$1" ]; then
+        if [ -e "$1" ]; then
+            if [ ! -d "$1" ]; then
+                print_error "$1 - a file with the same name already exists!"
+            else
+                print_success "$1"
+            fi
+        else
+            execute "mkdir -p $1" "$1"
+        fi
+    fi
+}
+
+print_error() {
+    # Print output in red
+    printf "\e[0;31m  [✖] $1 $2\e[0m\n"
+}
+
+print_info() {
+    # Print output in purple
+    printf "\n\e[0;35m $1\e[0m\n\n"
+}
+
+print_question() {
+    # Print output in yellow
+    printf "\e[0;33m  [?] $1\e[0m"
+}
+
+print_result() {
+    [ $1 -eq 0 ] \
+        && print_success "$2" \
+        || print_error "$2"
+
+    [ "$3" == "true" ] && [ $1 -ne 0 ] \
+        && exit
+}
+
+print_success() {
+    # Print output in green
+    printf "\e[0;32m  [✔] $1\e[0m\n"
+}
+
+
+
+
+
+
+#
+# actual symlink stuff
+#
+
+
+# finds all .dotfiles in this folder
+declare -a FILES_TO_SYMLINK=$(find . -type f -maxdepth 1 -name ".*" -not -name .DS_Store -not -name .git -not -name .macos | sed -e 's|//|/|' | sed -e 's|./.|.|' | sort)
+FILES_TO_SYMLINK="$FILES_TO_SYMLINK" # add in vim and the binaries
+
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+main() {
+    local i=""
+    local sourceFile=""
+    local targetFile=""
+
+    for i in ${FILES_TO_SYMLINK[@]}; do
+
+        sourceFile="$(pwd)/$i"
+        targetFile="$HOME/$(printf "%s" "$i" | sed "s/.*\/\(.*\)/\1/g")"
+
+        if [ -e "$targetFile" ]; then
+            if [ "$(readlink "$targetFile")" != "$sourceFile" ]; then
+
+                ask_for_confirmation "'$targetFile' already exists, do you want to overwrite it?"
+                if answer_is_yes; then
+                    rm -rf "$targetFile"
+                    execute "ln -fs $sourceFile $targetFile" "$targetFile → $sourceFile"
+                else
+                    print_error "$targetFile → $sourceFile"
+                fi
+
+            else
+                print_success "$targetFile → $sourceFile"
+            fi
+        else
+            execute "ln -fs $sourceFile $targetFile" "$targetFile → $sourceFile"
+        fi
+
+    done
+
+    # Delete any broken symlinks after things are run
+    find -L . -type l -maxdepth 1 | xargs rm
+}
+
+main
