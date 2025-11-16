89b8d5e8bc05e4e24fcbb31a59e0addbb7b790f9
Nicole <76132106+nicolecomputer@users.noreply.github.com>
2025-11-16 14:17:37 -0700

    lazygit

---
+++ install/20-lazygit.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+set -e
+
+TOOL_NAME="lazygit"
+
+install_tool() {
+    if command -v lazygit >/dev/null; then
+        return 0
+    fi
+
+    case "$PLATFORM" in
+        mac)
+            brew install lazygit
+            ;;
+        debian)
+            sudo apt install -y lazygit
+            ;;
+        fedora)
+            sudo dnf install -y lazygit
+            ;;
+        *)
+            echo "lazygit: unsupported platform $PLATFORM"
+            return 1
+            ;;
+    esac
+}
+
+install_tool
