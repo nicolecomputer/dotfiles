278babb2ba381f3ea443a7e680668af08e46a091
Nicole <76132106+nicolecomputer@users.noreply.github.com>
2025-11-16 13:44:34 -0700

    starship chrome hacks

--- install/20-starship.sh
+++ install/20-starship.sh
@@ -20,6 +20,8 @@ install_tool() {
 
             # If that fails, download binary from GitHub
             echo "starship not in repos, downloading binary..."
+            mkdir -p ~/.local/bin
+            EXPORT BIN_DIR="~/.local/bin"
             curl -sS https://starship.rs/install.sh | sh -s -- -y
             ;;
         fedora)
